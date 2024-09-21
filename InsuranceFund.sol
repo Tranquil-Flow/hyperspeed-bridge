0184087175627e8cc077360972df5fc8fa3fd6ea
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-09-21 11:43:36 +0800

    feat: init InsuranceFund

---
+++ InsuranceFund.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.8.0;
+
+// TODO: Implement a system for depositing and withdrawing funds.
+// TODO: Use deployed funds in a yield-generating strategy.
+// TODO: Add ability to liquidate funds in the case of a reorg, with funds going to the HypNative contract that was affected negatively.
+// TODO: Determine if deposited assets should be the native coin of the chain or a stablecoin.
+
+contract InsuranceFund {
+    constructor() {}
+}
